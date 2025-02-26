edab7e53f6724592cd4ec5f8264bcd480081b15a
Abdulhaseeb794 <asgharabdulhaseeb02@gmail.com>
2025-02-26 18:52:28 +0500

    Add files via upload

---
+++ string_1.py
@@ -0,0 +1,56 @@
+my_name = "Abdul"
+print(my_name[2])
+
+my_name2 = "Haseeb"
+print(my_name2[3])
+
+result = my_name + my_name2
+print(result)
+
+for i in my_name:
+    print(i)
+
+
+print('a' in 'program')   # True
+print('at' in 'battle')   # True
+print('batt' in 'battle')   # True
+print('batt' not in 'battle')   # False
+print('hass' not in 'hassan')   # False
+print('hasee' in 'haseeb')
+
+
+name = 'Hassan'
+country = 'Pakistan'
+
+print("my name is:", name, "and I am from:", country)
+
+print(f'my name is: {name} and I am from: {country}')
+
+s = "GeeksforGeeks"
+print(s[1:4])  
+print(s[:5])
+print(s[5:8])
+print(s[::-1])
+
+a = 3
+print(type(a))
+
+x = float(a)
+print(x)
+print(type(x))
+
+
+b = 4.5
+print(type(b))
+
+y = int(b)
+print(y)
+print(type(y))
+
+c = a + b
+print(c)
+print(type(c))
+
+c = x + y
+print(c)
+print(type(c))  
